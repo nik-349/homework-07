3295bef25b52ed8df88fde0cae572e1f0b159446
nik-349 <nik349@icloud.com>
2021-04-18 17:19:14 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,7 @@ const getMinNum = inputRef.getAttribute('data-length')
 const sl = inputRef.addEventListener('blur', callM)
 
 function callM(event) {
-    if(event.target.value.length >= minNum){
+    if(event.target.value.length >= getMinNum){
         inputRef.classList.remove('invalid')
         return inputRef.classList.add('valid')
     } return inputRef.classList.add('invalid')
